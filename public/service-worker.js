39b8ca680d9b5d1c8c504e127abfb8e2a8b4ff2c
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-02-10 14:33:46 -0800

    error-free completed PWA

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,18 +1,16 @@
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-const iconSizes = ["192", "512"];
-const iconFiles = iconSizes.map(
-    (size) => `/assets/images/icons/icon-${size}x${size}.png`
-);
-
 const staticFilesToPreCache = [
     "/",
     "/db.js",
+    "/index.js",
     "/index.html",
-    "/style.css",
+    "/styles.css",
     "/manifest.webmanifest",
-].concat(iconFiles);
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+]
 
 
 // install
